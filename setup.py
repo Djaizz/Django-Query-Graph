39d9ddbcea93ce71f003ef62082fb7979e85852c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-17 00:01:35 -0800

    update package metadata and setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,27 @@
-import os
-from ruamel import yaml
+import json
 from setuptools import find_packages, setup
 
 
-_PACKAGE_NAMESPACE_NAME = 'django_model_query_graphs'
-
-_METADATA_FILE_NAME = 'metadata.yml'
+_METADATA_FILE_NAME = 'metadata.json'
 
 _REQUIREMENTS_FILE_NAME = 'requirements.txt'
 
 
-_metadata = \
-    yaml.safe_load(
-        stream=open(os.path.join(
-                        os.path.dirname(__file__),
-                        _PACKAGE_NAMESPACE_NAME,
-                        _METADATA_FILE_NAME)))
-
+_metadata = json.load(open(_METADATA_FILE_NAME))
 
 setup(
     name=_metadata['PACKAGE'],
-    author=_metadata['AUTHOR'],
-    author_email=_metadata['AUTHOR_EMAIL'],
     url=_metadata['URL'],
     version=_metadata['VERSION'],
+    author=_metadata['AUTHOR'],
+    author_email=_metadata['AUTHOR-EMAIL'],
     description=_metadata['DESCRIPTION'],
     long_description=_metadata['DESCRIPTION'],
     keywords=_metadata['DESCRIPTION'],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=
-        [s
-         for s in {i.strip()
-                   for i in open(_REQUIREMENTS_FILE_NAME).readlines()}
-         if not s.startswith('#')])
+    install_requires=[s
+                      for s in {i.strip()
+                                for i in
+                                open(_REQUIREMENTS_FILE_NAME).readlines()}
+                      if not s.startswith('#')])
